de07d801f01d41acb977de036f2053924fa9667d
Neha Savant <ngs2116@columbia.edu>
2018-02-22 21:00:58 -0500

    updated setup.py to be fancier and ownery

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ setup(
     name="helloworld",
     version="0.1",
     packages=find_packages(),
+    author="Neha Savant",
+    license="GPLv3",
+    description="A package for saying hello and other things",
     entry_points={
         'console_scripts': ['helloworld = helloworld.__main__:main']
-        }
+        },
+    classifiers=["Programming Language :: Python :: 3"],
 )
